2406362294bc987a1e5a29269f2e42423c403ccf
Kevin Ahr <meowmeowahr@gmail.com>
2023-01-14 17:55:05 -0600

    Add PyQt version to about, Code Cleanup

--- about.py
+++ about.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python
 
+"""
+Kevinbot About App
+"""
+
 import json
 import platform
 import sys
@@ -21,24 +25,26 @@ EMULATE_REAL_REMOTE = True
 if platform.system() == "Windows":
     import ctypes
 
-    app_id = 'kevinbot.kevinbot.remote.about'  # arbitrary string
-    ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID(app_id)
+    WIN_APP_ID = 'kevinbot.kevinbot.remote.about'  # arbitrary string
+    ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID(WIN_APP_ID)
 
-settings = json.load(open("settings.json", "r"))
+settings = json.load(open("settings.json", encoding="utf-8"))
 
 haptics.init(21)
 
 
 class MainWindow(QMainWindow):
+    """ Kevinbot About App Window """
     # noinspection PyUnresolvedReferences,PyArgumentList
     def __init__(self):
         # noinspection PyArgumentList
         super().__init__()
         self.setWindowTitle("About Kevinbot Remote")
         self.setObjectName("Kevinbot3_RemoteUI")
-        
+
         try:
-            load_theme(self, settings["window_properties"]["theme"], settings["window_properties"]["theme_colors"])
+            load_theme(self, settings["window_properties"]["theme"],
+                       settings["window_properties"]["theme_colors"])
         except NameError:
             load_theme(self, settings["window_properties"]["theme"])
 
@@ -48,7 +54,8 @@ class MainWindow(QMainWindow):
 
         self.ensurePolished()
         if detect_dark((QColor(self.palette().color(QPalette.Window)).getRgb()[0],
-                        QColor(self.palette().color(QPalette.Window)).getRgb()[1],
+                        QColor(self.palette().color(
+                            QPalette.Window)).getRgb()[1],
                         QColor(self.palette().color(QPalette.Window)).getRgb()[2])):
             self.fg_color = Qt.GlobalColor.white
         else:
@@ -75,17 +82,23 @@ class MainWindow(QMainWindow):
         self.name_text.setAlignment(Qt.AlignmentFlag.AlignCenter)
         self.layout.addWidget(self.name_text)
 
-        self.version = QLabel("Version: " + open("version.txt").read())
+        self.version = QLabel("Version: " + open("version.txt", encoding="utf-8").read())
         self.version.setStyleSheet("font-size: 24px; font-weight: semibold;")
         self.version.setAlignment(Qt.AlignmentFlag.AlignCenter)
         self.layout.addWidget(self.version)
+
+        self.qt_version = QLabel("PyQt Version: " + PYQT_VERSION_STR)
+        self.qt_version.setStyleSheet("font-size: 22px; font-weight: normal;")
+        self.qt_version.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.layout.addWidget(self.qt_version)
 
         self.credits_box = QTextEdit()
         self.credits_box.setReadOnly(True)
         self.credits_box.setText("<p>Kevinbot and Kevinbot Software created by Kevin Ahr <br>"
                                  "PyQtDarkTheme created by Yunosuke Ohsugi <br>"
                                  "BreezeStyleSheets created by Alexander Huszagh </p>")
-        self.credits_box.setVerticalScrollBarPolicy(Qt.ScrollBarPolicy.ScrollBarAlwaysOff)
+        self.credits_box.setVerticalScrollBarPolicy(
+            Qt.ScrollBarPolicy.ScrollBarAlwaysOff)
         self.credits_box.hide()
         self.layout.addWidget(self.credits_box)
 
@@ -102,27 +115,34 @@ class MainWindow(QMainWindow):
         self.close_button.clicked.connect(self.close)
         self.button_layout.addWidget(self.close_button)
 
+        self.open_animation = QPropertyAnimation(self.credits_box, b"maximumHeight")
+        self.open_animation.setStartValue(0)
+        self.open_animation.setEndValue(100)
+        self.open_animation.setDuration(100)
+
+        self.close_animation = QPropertyAnimation(self.credits_box, b"maximumHeight")
+        self.close_animation.setStartValue(100)
+        self.close_animation.setEndValue(0)
+        self.close_animation.setDuration(100)
+        self.close_animation.start()
+        self.close_animation.finished.connect(self.credits_box.hide)
+
         if START_FULL_SCREEN:
             self.showFullScreen()
         else:
             self.show()
 
     def toggle_credits(self):
+        """
+        Show/Hide Credits Panel
+        """
+
         if self.credits.isChecked():
             self.credits_box.show()
-            self.anim = QPropertyAnimation(self.credits_box, b"maximumHeight")
-            self.anim.setStartValue(0)
-            self.anim.setEndValue(100)
-            self.anim.setDuration(100)
-            self.anim.start()
+            self.open_animation.start()
         else:
             # noinspection PyAttributeOutsideInit
-            self.anim = QPropertyAnimation(self.credits_box, b"maximumHeight")
-            self.anim.setStartValue(100)
-            self.anim.setEndValue(0)
-            self.anim.setDuration(100)
-            self.anim.start()
-            self.anim.finished.connect(self.credits_box.hide)
+            self.close_animation.start()
 
 
 if __name__ == "__main__":
