3e7bc0cdbccf428143f6fc204f71c195565a52c0
Kevin Ahr <meowmeowahr@gmail.com>
2023-02-14 10:35:39 -0600

    🪲 Fix runner.py bug

--- runner.py
+++ runner.py
@@ -4,7 +4,7 @@ import os
 from utils import is_pi
 
 if __name__ == "__main__":
-    os.system("./menu.py")
+    os.system("python3 menu.py")
     if is_pi():
-        print("Looks like something went wrong\nRebooting Now")
+        print("Looks like something went wrong")
         # os.system("sudo shutdown now -r")
