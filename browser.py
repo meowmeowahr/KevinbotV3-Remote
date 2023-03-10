cc294b382b248aac6583ea87bc3b0c8a0942b427
Kevin Ahr <meowmeowahr@gmail.com>
2023-03-10 13:16:27 -0600

    Convert PyQt5 to qtpy in browser.py

--- browser.py
+++ browser.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python
 
-from PyQt5.QtCore import *
-from PyQt5.QtWidgets import *
-from PyQt5.QtGui import *
-from PyQt5.QtWebEngineWidgets import *
+from qtpy.QtCore import *
+from qtpy.QtWidgets import *
+from qtpy.QtGui import *
+from qtpy.QtWebEngineWidgets import *
 from QCustomWidgets import KBMainWindow
 import qtawesome as qta
 
